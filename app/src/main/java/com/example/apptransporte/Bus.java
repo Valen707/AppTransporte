17ea827f00f9dd405edfa7efd0aad97f4c0a9953
Valen707 <lorenzan456@gmail.com>
2023-05-02 19:45:38 -0500

    Agrego los campos de Bus

--- app/src/main/java/com/example/apptransporte/Bus.java
+++ app/src/main/java/com/example/apptransporte/Bus.java
@@ -2,13 +2,29 @@ package com.example.apptransporte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Bus extends AppCompatActivity {
+
+    EditText CampDestino;
+    EditText CampTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus);
+
+        CampDestino = findViewById(R.id.TextDestino);
+        CampTime = findViewById(R.id.TextTime);
+    }
+
+    public void confirmarBus(View b){
+        Intent ir = new Intent(this, ConfirmacionBus.class);
+        ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TASK | ir.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(ir);
+
     }
 }
